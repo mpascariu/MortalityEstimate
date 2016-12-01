02de888aa8ac7d8a998b0835b55510d485e589f2
mpascariu <xpascariu@outlook.com>
2016-12-01 14:17:42 +0100

    Rotated vx implementation (as in Li et al. 2013 + logistic function for old ages)

--- R/fun_Generics.R
+++ R/fun_Generics.R
@@ -41,12 +41,59 @@ print.LinearLink <- function(x, ...){
   
 #' @keywords internal
 #' @export
-predict.LinearLink <- function(object, ex_target, ...) {
-  coefs <- data.frame(coef(object)$bx, coef(object)$vx, 
+predict.LinearLink <- function(object, ex_target, 
+                               use.vx.rotation = FALSE, ...) {
+  # Choose vx coefficients
+  vx_o <- coef(object)$vx # original vx coefficients
+  vx_r <- rotated_vx(object, e0_t = ex_target) # rotated vx coeffs
+  if (use.vx.rotation == TRUE) {vx = vx_r} else {vx = vx_o}
+  # Data.frame with all coefficients used in prediction
+  coefs <- data.frame(bx = coef(object)$bx, vx = vx, 
                       row.names = object$input$mx_ages)
+  # Find the right life table
   pred.values <- FUN.lt_optim(ages = object$input$mx_ages, 
-                             coefs = coefs, ex0 = ex_target)
+                              coefs = coefs, ex0 = ex_target)
+  pred.values$bx <- coefs$bx
+  pred.values$vx <- coefs$vx
   return(pred.values)
+}
+
+#' @keywords internal
+#' 
+rotated_vx <- function(object, e0_t, e0_u = 102, 
+                       e0_threshold = 80, p_ = 0.5){
+  vx = coef(object)$vx
+  x  = object$input$mx_ages
+  x1 = max(min(x), 15):65
+  x2 = 66:max(x)
+  
+  vx_u     = vx * 0
+  vx_young = mean(vx[x1 + 1])
+  n_vx     = length(vx[x2 + 1])
+  # Derive a logistic shape. The values have to be between 0 and 1, 
+  # they will be scaled.
+  x_num = seq(-6, 6, length.out = n_vx)
+  logit_shape = 1 - exp(x_num)/ (1 + exp(x_num)) 
+  vx_old_ = logit_shape * vx_young # scale values
+  # This is our new vx
+  vx_u[min(x):max(x1 + 1)] = vx_young  # we use only the values between age 15 and 65
+  vx_u[x2 + 1] <- vx_old_
+  
+  # Compute weights
+  w_t  <- (e0_t - e0_threshold)/(e0_u - e0_threshold)
+  ws_t <- (0.5 * (1 + sin(pi/2 * (2*w_t - 1))) ) ^ p_ 
+  # The power to the smooth-weight function, p_, takes values
+  # between 0 and 1, which makes the rotation faster at 
+  # starting times and slower at ending times. 
+  # In this article, p = .5 is taken as the default. (Li et al. 2013)
+  
+  # Compute rotate_vx
+  if ( e0_t < e0_threshold ) {rot_vx = vx }
+  if ( e0_threshold <= e0_t & e0_t < e0_u ) {
+    rot_vx = (1 - ws_t) * vx + ws_t * vx_u
+  }
+  if (e0_t >= e0_u) {rot_vx = vx_u}
+  return(rot_vx)
 }
 
 # ==========================================================================
