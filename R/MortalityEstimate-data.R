8b76c43e793ff67342d0255f5c49560ebdf4c8f3
Marius D. Pascariu <rpascariu@outlook.com>
2020-10-01 23:11:56 +0200

    v0.9.8 - Update HMD data to be used in the LinearLink examples as in the article

--- R/MortalityEstimate-data.R
+++ R/MortalityEstimate-data.R
@@ -1,20 +1,21 @@
 # --------------------------------------------------- #
 # Author: Marius D. Pascariu
-# License: GNU General Public License v3.0
-# Last update: Sat Dec  1 21:20:51 2018
+# License: MIT
+# Last update: Wed Sep 30 10:02:58 2020
 # --------------------------------------------------- #
 
 # Data in the package ----
 
 
-#' Death Rates for Female Populations in England and Wales, France, Sweden and USA 
+#' Death Rates for Female Populations in England and Wales, France, Sweden 
+#' and USA 
 #'
 #' Dataset containing containing 4 matrices with death rates for 
 #' female populations in England and Wales, France, Sweden and USA 
-#' between 1965 and 2014. This data is provided for testing purposes only. 
+#' between 1965 and 2018. This data is provided for testing purposes only. 
 #' It may not be up to date anymore. Download the actual data free of charge 
-#' from \url{http://www.mortality.org}.  
-#' @source Human Mortality Database, \url{http://www.mortality.org}.
+#' from \url{https://www.mortality.org}.  
+#' @source Human Mortality Database, \url{https://www.mortality.org}.
 "HMD4mx"
 
 #' Print function for HMD4mx data
@@ -27,22 +28,23 @@ print.MortalityEstimateData <- function(x, ...) {
   cat("\nMortalityEstimate Test Data\n")
   cat(" Populations : England and Wales, France, Sweden, USA\n")
   cat(" Series      : Death rates for females (raw)\n")
-  cat(" Years       : 1965 - 2014\n")
+  cat(" Years       : 1965 - 2018\n")
   cat(" Ages        : 0 - 110\n")
   cat(" Format      : List containg 4 data frames\n")
   cat(" Source      : Human Mortality Database\n")
-  cat(" Download    : December 7, 2017")
+  cat(" Download    : September 30, 2020")
 }
 
 
 #' 719 Life Tables from HMD 
 #' 
-#' Data used in the Wilmoth et. al. (2012) article. Today this dataset is outdated,
-#' download the actual data free of charge from \url{http://www.mortality.org}. 
+#' Data used in the Wilmoth et. al. (2012) article. Today this dataset is 
+#' outdated. Download the actual data free of charge from 
+#' \url{https://www.mortality.org}. 
 #' @references John Wilmoth, Sarah Zureick, Vladimir Canudas-Romo, Mie Inoue & 
 #' Cheryl Sawyer (2012): A flexible two-dimensional mortality model for use in 
 #' indirect estimation, Population Studies: A Journal of Demography, 66:1, 1-28
 #' \url{http://dx.doi.org/10.1080/00324728.2011.611411}
-#' @source Human Mortality Database, \url{http://www.mortality.org}. (2012)
+#' @source Human Mortality Database, \url{https://www.mortality.org}. (2012)
 "HMD719"
 
--- data-raw/addDataInPackage.R
+++ data-raw/addDataInPackage.R
@@ -1,10 +1,9 @@
 # --------------------------------------------------- #
 # Author: Marius D. Pascariu
 # License: MIT
-# Last update: Tue Jun 18 14:45:29 2019
+# Last update: Wed Sep 30 10:02:26 2020
 # --------------------------------------------------- #
 remove(list = ls())
-
 library(dplyr)
 library(MortalityLaws)
 
@@ -16,21 +15,33 @@ cntr = c("GBRTENW", "FRATNP", "SWE", "USA")
 HMDuser = "..."
 HMDpass = "..."
 
-HMD_Dx <- ReadHMD(what = "Dx", countries = cntr,
-                  username = HMDuser, password = HMDpass,
-                  save = F)$data[, 1:4]
-HMD_Ex <- ReadHMD(what = "Ex", countries = cntr,
-                  username = HMDuser, password = HMDpass,
-                  save = F)$data[, 1:4]
+HMD_Dx <- ReadHMD(
+            what = "Dx", 
+            countries = cntr,
+            username = HMDuser, 
+            password = HMDpass,
+            save = FALSE)$data[, 1:4]
+
+HMD_Ex <- ReadHMD(
+            what = "Ex", 
+            countries = cntr,
+            username = HMDuser, 
+            password = HMDpass,
+            save = F)$data[, 1:4]
+
 HMD_mx <- data.frame(HMD_Dx[, 1:3], mx = HMD_Dx[, 4]/HMD_Ex[, 4])
 
 build.mx.wide <- function(HMD_mx, years, ages) {
   HMDmx <- HMD_mx %>% filter(Year %in% years, Age %in% ages)
   out   <- list()
   for (i in 1:length(cntr)) {
-    mx_i <- HMDmx %>% filter(country == cntr[i]) %>% 
-             reshape(direction = 'wide', idvar = c('country','Age'), timevar = 'Year') %>% 
-             select(-(country:Age))
+    mx_i <- HMDmx %>% 
+      filter(country == cntr[i]) %>% 
+      reshape(
+        direction = 'wide', 
+        idvar = c('country','Age'), 
+        timevar = 'Year') %>% 
+      select(-(country:Age))
     dimnames(mx_i) <- list(ages, years)
     out[[i]] <- mx_i
   }
@@ -39,8 +50,7 @@ build.mx.wide <- function(HMD_mx, years, ages) {
 }
 
 
-
-years = 1965:2016
+years = 1965:2018
 ages  = 0:110
 dta  <- build.mx.wide(HMD_mx, years, ages)
 HMD4mx <- structure(class = "MortalityEstimateData", dta)
